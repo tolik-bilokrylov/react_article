61ff6cc48219ddcf6dad797f12029be868eb607d
Tolik Bilokrylov <enkorn1715@gmail.com>
2021-07-15 12:21:11 +0300

    Updates

---
+++ precache-manifest.8568ec6bceeff09d785d83e6834770af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462111b2d9804b0c2ab5429e6b26f75a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d250cb53dd501e80f2",
+    "url": "./static/js/main.dd963318.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
